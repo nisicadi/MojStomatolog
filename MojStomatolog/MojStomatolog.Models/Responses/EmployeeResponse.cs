36efe0e0d49d576186ebea40439ff5ee7461105f
Adi Nišić <45313870+nisicadi@users.noreply.github.com>
2024-01-04 20:50:55 +0100

    Added Employee DTOs

---
+++ MojStomatolog/MojStomatolog.Models/Responses/EmployeeResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace MojStomatolog.Models.Responses
+{
+    public class EmployeeResponse
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public char Gender { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Number { get; set; } = null!;
+
+        public string Specialization { get; set; } = null!;
+
+        public DateTime StartDate { get; set; }
+    }
+}
